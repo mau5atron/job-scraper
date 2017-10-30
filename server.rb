ec3f65ad115f9c2655f97893f9094af3a6de1715
mau5atron <gabrielbetancourt96@outlook.com>
2017-10-29 23:47:38 -0400

    corrected server.rb's httparty

--- server.rb
+++ server.rb
@@ -1,21 +1,15 @@
 require 'sinatra'
 require 'nokogiri'
-require 'HTTParty'
+require 'httparty'
 
 get '/' do
 
-url = "https://miami.craigslist.org/search/sof"
-response = HTTParty.get url
+	url = "https://miami.craigslist.org/search/sof"
+	response = HTTParty.get url
 
-dom = Nokogiri::HTML(response.body)
-
-
-info = dom.css('.content').css('.result-row').css('.hdrlnk').map do |a|
+	dom = Nokogiri::HTML(response.body)
+	
+	info = dom.css('.content').css('.result-row').css('.hdrlnk').map do |a|
   "<p>#{a.text}</p>"
-
-end
-
-
-
-
+	end
 end
